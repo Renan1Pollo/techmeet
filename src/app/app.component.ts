2656b14c4766262e80ce33cab263ca54c1c085e4
Renan Pollo Benelli <renansk18@outlook.com>
2024-05-27 23:59:46 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { EventComponent } from './components/event/event.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, EventComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  events = [
+    { title: "Frontin Sampa", place: "São Paulo", date: "19/10/2024", description: "Maior evento de Frontend do Brasil!"}
+  ]
+  title: any;
+}
